9496764c500033f8270d2b607555b6aaefeb1697
beepscore <support@beepscore.com>
2012-02-11 10:59:19 -0800

    Exercise 1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+print "Hello World!"
+#print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# Extra Credit
+# character in column 0 starts a comment. Called number sign, pound sign, hash, or octothorpe.
+print "Printing another line."
+
