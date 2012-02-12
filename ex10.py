0f8c377752876c821fa79d0aebb980ab9dc80148
beepscore <support@beepscore.com>
2012-02-11 20:59:10 -0800

    Print with %s format specifier. Output looks same as no specifier.

--- ex10.py
+++ ex10.py
@@ -8,21 +8,32 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
-I'll do a list:
-    \t* Cat food
-    \t* Fishies
-    \t* Catnip\n\t* Grass
-    """
-
-
 # When an 'r' or 'R' prefix is present, a character following a backslash is included in the string without change, and all backslashes are left in the string.
 print "%r" % tabby_cat
 print tabby_cat
 print "%r" % persian_cat
 print persian_cat
 print "%r" % backslash_cat
+print "%s" % backslash_cat
 print backslash_cat
+
+
+fat_cat = """
+I'll do a list:
+    \t* Cat food
+    \t* Fishies
+    \t* Catnip\n\t* Grass
+    """
+print "%r" % fat_cat
+print "%s" % fat_cat
+print fat_cat
+
+fat_cat = '''
+I'll do a list:
+    \t* Cat food
+    \t* Fishies
+    \t* Catnip\n\t* Grass
+   ''' 
 print "%r" % fat_cat
 print fat_cat
 
