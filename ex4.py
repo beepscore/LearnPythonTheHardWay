aa2b8bbd8667a0927ec551b30f3bebea741ac43c
beepscore <support@beepscore.com>
2012-02-11 16:52:56 -0800

    import math module and print math.pi

--- ex4.py
+++ ex4.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import math
+
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
@@ -15,3 +17,5 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpool today."
 print "We neet to put about", average_passengers_per_car, "in each car."
+
+print "math.pi =", math.pi
