5fa26ec0c5403c63113e160d6b265e10e174d916
beepscore <support@beepscore.com>
2012-02-12 10:16:23 -0800

    Exercise 13.

---
+++ ex13.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# References
+
+#import sys module
+from sys import argv
+
+# argv is "argument variable". Holds the script's arguments
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your fist variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third 
