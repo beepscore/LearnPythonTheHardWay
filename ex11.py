f156827db6a4ba3454d3dd520b8f4415750052fa
beepscore <support@beepscore.com>
2012-02-11 21:28:35 -0800

    Use %s format specifier to suppress printing escape characters.

--- ex11.py
+++ ex11.py
@@ -2,6 +2,7 @@
 
 # References
 # python raw input
+# http://docs.python.org/library/functions.html
 
 print "How old are you?",
 age = raw_input()
@@ -11,5 +12,8 @@ print "How much do you weigh?",
 weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (
+        age, height, weight)
+# use %s format specifier to suppress printing escape characters
+print "So, you're %s old, %s tall and %s heavy." % (
         age, height, weight)
 
