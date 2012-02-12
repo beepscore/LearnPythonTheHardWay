d82b5e2ea9c2a590952c562f8c40bccdd7e00174
beepscore <support@beepscore.com>
2012-02-11 19:53:10 -0800

    Extra credit.

--- ex6.py
+++ ex6.py
@@ -4,23 +4,32 @@
 # string formatting
 # http://docs.python.org/library/stdtypes.html
 
+# put string in string #6 (converts int to string)
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
+# put string in string #1, #2
 y = "Those who know %s and those who %s." % (binary, do_not)
 
 print x
 print y
 
+# use %r format specifier. It puts the value of x within ' '
+# put string in string #3
 print "I said: %r." % x
+# put string in string #4
 print "I also said: '%s'." % y
 
 hilarious = False
+# put string in string #5 (converts bool to string)
 joke_evaluation = "Isn't that joke so funny?! %r"
 
+# joke_evaluation contains format specifier, 
+# so it works with % hilarious
 print joke_evaluation % hilarious
 
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# concatenate strings
 print w + e
