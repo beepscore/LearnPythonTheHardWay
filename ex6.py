1b7bfa9ab6c2f382c0b3b8931a16af3e79540001
beepscore <support@beepscore.com>
2012-02-11 19:31:23 -0800

    Exercise 6 continued.

--- ex6.py
+++ ex6.py
@@ -13,3 +13,14 @@ print x
 print y
 
 print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
