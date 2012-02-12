1cf5bb74c72745d94326c14572c86b9a34e961a6
beepscore <support@beepscore.com>
2012-02-11 20:49:21 -0800

    Add print statements that use %r format specifier.

--- ex10.py
+++ ex10.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# References
+# python escape sequences
+# http://docs.python.org/reference/lexical_analysis.html
+
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
@@ -11,8 +15,14 @@ I'll do a list:
     \t* Catnip\n\t* Grass
     """
 
+
+# When an 'r' or 'R' prefix is present, a character following a backslash is included in the string without change, and all backslashes are left in the string.
+print "%r" % tabby_cat
 print tabby_cat
+print "%r" % persian_cat
 print persian_cat
+print "%r" % backslash_cat
 print backslash_cat
+print "%r" % fat_cat
 print fat_cat
 
