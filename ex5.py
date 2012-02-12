55f0d2f30bd714d8357163c9601b717e92d91c90
beepscore <support@beepscore.com>
2012-02-11 17:46:49 -0800

    Change constant to uppercase, use LBM.

--- ex5.py
+++ ex5.py
@@ -5,7 +5,7 @@
 # http://docs.python.org/library/stdtypes.html
 # python doesn't have constants. You can make a read-only property.
 # http://stackoverflow.com/questions/2682745/creating-constant-in-python
-k_pounds_per_kilogram = 2.204
+LBM_PER_KG = 2.204
 
 name = 'Zed A. Shaw'
 age = 35 # not a lie
@@ -18,7 +18,7 @@ hair_color = 'Brown'
 print "Let's talk about %s." % name
 print "He's %d inches tall." % height
 print "He's %d pounds heavy." % weight_lb
-print "He's %g kilograms heavy." % (weight_lb / k_pounds_per_kilogram)
+print "He's %g kilograms heavy." % (weight_lb / LBM_PER_KG)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eye_color, hair_color)
 print "His teeth are usually %s depending on the coffee." % teeth
