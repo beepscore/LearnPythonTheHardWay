b8ba3c130c85e040ae23355000442e77952ff1a2
beepscore <support@beepscore.com>
2012-03-10 18:23:57 -0800

    Change confusing variable names.

--- ex18.py
+++ ex18.py
@@ -4,18 +4,17 @@
 def print_two(*args):
     print "args[0]: %r, args[1]: %r" % (args[0], args[1])
     #In Python can assign multiple variables from one array on one line
-    # assign arg1 and arg2 using array "args"
-    # These names are confusing! arg1 == args[0] and arg2 == args[1]
-    arg1, arg2 = args
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+    # assign arg0 and arg1 using array "args"
+    arg0, arg1 = args
+    print "arg0: %r, arg1: %r" % (arg0, arg1)
 
 # ok, that *args is actually pointless, we can just do this
-def print_two_again(arg1, arg2):
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+def print_two_again(arg0, arg1):
+    print "arg0: %r, arg1: %r" % (arg0, arg1)
 
 # this takes one argument
-def print_one(arg1):
-    print "arg1: %r" % arg1
+def print_one(arg0):
+    print "arg0: %r" % arg0
 
 # this one takes no arguments
 def print_none():
