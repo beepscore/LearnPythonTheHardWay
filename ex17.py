3fdb7325dac5d790e196b634ec1b0b4f7ed69b5a
beepscore <support@beepscore.com>
2012-03-10 17:11:37 -0800

    ex17.py extra credit. Delete unneeded prompt

--- ex17.py
+++ ex17.py
@@ -17,8 +17,6 @@ indata = input.read()
 print "The input file is %d bytes long" % len(indata)
 
 print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
 
 output = open(to_file, 'w')
 output.write(indata)
